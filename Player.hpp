55c822a6b114d960b326b0c5232f836685aee1b4
darubagus <darubagus@gmail.com>
2021-03-14 21:38:17 +0700

    Create Player.hpp

---
+++ Player.hpp
@@ -0,0 +1,46 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Player {
+    private :
+        // Position playerPos;
+        // Engimon active;
+        // Inventory playerInventory;
+        int activeIndex;
+
+    public :
+        Player();
+        // default constructor for player
+        ~Player();
+        // dtor
+
+        // Commands
+        void Move();
+        void changeEngimon();
+        // Engimon Breed();
+        void Battle();
+        // void checkEngimon();
+        // void useItem(Item);
+        // Interact
+
+        // display
+        void displayEngimon();
+        void displaySkillItem();
+        void detailEngimon();
+
+        // Getter Setter
+        // Engimon getActiveEngimon();
+        // Position getPosition();
+        int getActiveIndex();
+        void setActiveEngimon();
+        void setPosition();
+        void setActiveIndex();
+
+};
+
+#endif
